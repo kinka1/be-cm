1e363f1cdc4745a8f0632167c7f2f287cb8fda01
Nufus Akmalul <nufusmafaza@gmail.com>
2026-08-03 06:06:20 +0700

    feat: add JeruChaSeeder for store and product initialization

---
+++ database/seeders/JeruChaSeeder.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Category;
+use App\Models\Product;
+use App\Models\Store;
+use Illuminate\Database\Seeder;
+
+class JeruChaSeeder extends Seeder
+{
+    public function run(): void
+    {
+        $store = Store::query()->updateOrCreate(
+            ['code' => 'JeruCha'],
+            [
+                'store_name' => 'JeruCha',
+                'address' => 'Jalan Tuk Buntung, Sidomulyo, Cepu, Kabupaten Blora, Jawa Tengah (depan bekas SMK PGRI Cepu).',
+                'phone' => '0812345678',
+                'is_active' => true,
+            ]
+        );
+
+        $categories = collect($this->menus())
+            ->keys()
+            ->mapWithKeys(function (string $categoryName) use ($store): array {
+                $category = Category::query()->updateOrCreate(
+                    [
+                        'store_id' => $store->id,
+                        'category_name' => $categoryName,
+                    ],
+                    ['description' => null]
+                );
+
+                return [$categoryName => $category];
+            });
+
+        foreach ($this->menus() as $categoryName => $products) {
+            foreach ($products as $product) {
+                Product::query()->updateOrCreate(
+                    ['sku' => $product['sku']],
+                    [
+                        'store_id' => $store->id,
+                        'category_id' => $categories[$categoryName]->id,
+                        'product_name' => $product['name'],
+                        'description' => null,
+                        'unit_of_measure' => 'pcs',
+                        'minimum_stock' => 0,
+                        'current_stock' => 0,
+                        'cost_price' => 0,
+                        'selling_price' => $product['price'],
+                        'is_active' => true,
+                    ]
+                );
+            }
+        }
+    }
+
+    private function menus(): array
+    {
+        return [
+            'Matcha Series' => [
+                ['name' => 'Matcha OG', 'sku' => 'JERUCHA-MATCHA-OG', 'price' => 15000],
+                ['name' => 'Matcha Berry', 'sku' => 'JERUCHA-MATCHA-BERRY', 'price' => 16000],
+                ['name' => 'Honey Matcha', 'sku' => 'JERUCHA-HONEY-MATCHA', 'price' => 17000],
+                ['name' => 'Choco Matcha', 'sku' => 'JERUCHA-CHOCO-MATCHA', 'price' => 18000],
+                ['name' => 'Matcha Cookies', 'sku' => 'JERUCHA-MATCHA-COOKIES', 'price' => 18000],
+            ],
+            'Jeruk Series' => [
+                ['name' => 'Jeruk Ori', 'sku' => 'JERUCHA-JERUK-ORI', 'price' => 8000],
+                ['name' => 'Jeruk Madu', 'sku' => 'JERUCHA-JERUK-MADU', 'price' => 10000],
+                ['name' => 'Jeruk Yakult', 'sku' => 'JERUCHA-JERUK-YAKULT', 'price' => 11000],
+            ],
+            'Coffee Series' => [
+                ['name' => 'Americano', 'sku' => 'JERUCHA-AMERICANO', 'price' => 15000],
+                ['name' => 'Cafe Latte', 'sku' => 'JERUCHA-CAFE-LATTE', 'price' => 17000],
+                ['name' => 'Aren Latte', 'sku' => 'JERUCHA-AREN-LATTE', 'price' => 17000],
+                ['name' => 'Vanilla Latte', 'sku' => 'JERUCHA-VANILLA-LATTE', 'price' => 20000],
+                ['name' => 'Butterscotch Latte', 'sku' => 'JERUCHA-BUTTERSCOTCH-LATTE', 'price' => 20000],
+                ['name' => 'Mochaccino', 'sku' => 'JERUCHA-MOCHACCINO', 'price' => 21000],
+            ],
+            'Non Coffee Series' => [
+                ['name' => 'Chocolate', 'sku' => 'JERUCHA-CHOCOLATE', 'price' => 15000],
+                ['name' => 'Red Velvet', 'sku' => 'JERUCHA-RED-VELVET', 'price' => 15000],
+                ['name' => 'Milky Berry', 'sku' => 'JERUCHA-MILKY-BERRY', 'price' => 15000],
+                ['name' => 'Cookies N Cream', 'sku' => 'JERUCHA-COOKIES-N-CREAM', 'price' => 15000],
+            ],
+            'Toast JeruCha' => [
+                ['name' => 'Ori Manis', 'sku' => 'JERUCHA-ORI-MANIS', 'price' => 7000],
+                ['name' => 'Toast Coklat', 'sku' => 'JERUCHA-TOAST-COKLAT', 'price' => 9000],
+                ['name' => 'Toast Keju', 'sku' => 'JERUCHA-TOAST-KEJU', 'price' => 9000],
+                ['name' => 'Toast Coklat Keju', 'sku' => 'JERUCHA-TOAST-COKLAT-KEJU', 'price' => 11000],
+            ],
+            'Other JeruCha' => [
+                ['name' => 'Mix Platter', 'sku' => 'JERUCHA-MIX-PLATTER', 'price' => 15000],
+            ],
+            'Tambahan' => [
+                ['name' => 'Extra Shot', 'sku' => 'JERUCHA-EXTRA-SHOT', 'price' => 5000],
+            ],
+        ];
+    }
+}
